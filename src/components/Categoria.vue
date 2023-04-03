047b5d4c910bc1982b7bddbc017129910615f0e7
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-04-03 17:41:44 -0600

    agregar tabla crud categorias segun video

--- src/components/Categoria.vue
+++ src/components/Categoria.vue
@@ -6,95 +6,74 @@
         <v-divider class="mx-2" inset vertical></v-divider>
 
         <v-spacer></v-spacer>
-        
-        <v-text-field
-          class="text-xs-center"
-          v-model="search"
-          append-icon="search"
-          label="Búsqueda"
-          single-line
-          hide-details
-        ></v-text-field>
+
+        <v-text-field class="text-xs-center" v-model="search" append-icon="search" label="Búsqueda" single-line
+          hide-details></v-text-field>
 
         <v-spacer></v-spacer>
-        
+
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on }">
             <v-btn color="primary" dark class="mb-2" v-on="on">Nuevo</v-btn>
           </template>
+
           <v-card>
             <v-card-title>
               <span class="headline">{{ formTitle }}</span>
             </v-card-title>
-
             <v-card-text>
               <v-container grid-list-md>
                 <v-layout wrap>
                   <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Dessert name"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
                   </v-flex>
                   <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      v-model="editedItem.calories"
-                      label="Calories"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
                   </v-flex>
                   <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
                   </v-flex>
                   <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
                   </v-flex>
                   <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
+                    <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
                   </v-flex>
                 </v-layout>
               </v-container>
             </v-card-text>
-
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue darken-1" class="white--text" flat @click="close">Cancelar</v-btn>
               <v-btn color="blue darken-1" class="white--text" flat @click="save">Guardar</v-btn>
             </v-card-actions>
           </v-card>
+
         </v-dialog>
-        
       </v-toolbar>
 
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :search="search"
-        class="elevation-1"
-      >
+      <v-data-table :headers="headers" :items="categorias" :search="search" class="elevation-1">
         <template v-slot:items="props">
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.calories }}</td>
-          <td class="text-xs-right">{{ props.item.fat }}</td>
-          <td class="text-xs-right">{{ props.item.carbs }}</td>
-          <td class="text-xs-right">{{ props.item.protein }}</td>
-          <td class="justify-center layout px-0">
-            <v-icon small class="mr-2" @click="editItem(props.item)">
-              edit
-            </v-icon>
-            <v-icon small @click="deleteItem(props.item)"> delete </v-icon>
+          <td>{{ props.item.nombre }}</td>
+          <td>{{ props.item.descripcion }}</td>
+          <td>
+            <div v-if="prop.item.estado">
+              <span class="blue--text">Activo</span>
+            </div>
+            <div v-else="prop.item.estado">
+              <span class="red--text">Inactivo</span>
+            </div>
+          </td>
+        </template>
+        <template v-slot:item.opciones="{ item }"> <!-- usa los iconos en el header opciones -->
+          <td>
+            <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
           </td>
         </template>
         <template v-slot:no-data>
-          <v-btn color="primary" @click="initialize">Reset</v-btn>
+          <v-btn color="primary" @click="listar()">Resetear</v-btn>
+          <!-- Si no hay valores se muestra un boton de -->
         </template>
       </v-data-table>
     </v-flex>
@@ -102,25 +81,19 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       dialog: false,
       search: "",
+      categorias: [],
       headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "left",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Actions", value: "name", sortable: false },
+        { text: 'Opciones', value: 'opciones', sortable: false },
+        { text: "Nombre", value: "nombre", sortable: true },
+        { text: "Descripción", value: "descripcion", sortable: false },
+        { text: "Estado", value: "estado", sortable: false },
       ],
-      desserts: [],
       editedIndex: -1,
       editedItem: {
         name: "",
@@ -149,19 +122,20 @@ export default {
     },
   },
   created() {
-    this.initialize();
+    this.listar();
   },
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-        },
-      ];
+    listar() {
+      let me = this;
+      axios
+        .get("categoria/list")
+        .then(function (response) {
+          //console.log(response);
+          me.categorias = response.data; //Se asigna las categorias que se obtienen del response en data en el array categorias
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     },
 
     editItem(item) {
