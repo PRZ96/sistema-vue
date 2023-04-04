bc8cc55f59821f486545db5797fdf07268fab0a2
Pablo Perez <pablo.ceti.96@hotmail.com>
2023-04-04 11:52:25 -0600

    configurar el metodo create del crud

--- src/components/Categoria.vue
+++ src/components/Categoria.vue
@@ -2,98 +2,67 @@
   <v-layout align-start class="mt-6">
     <v-flex>
 
-      <v-data-table
-      :headers="headers"
-      :items="categorias"
-      :search = "search"
-      class="elevation-1"
-    >
-      <template v-slot:top>
-        <v-toolbar flat color="white">
-          <v-toolbar-title>Categorías</v-toolbar-title>
-          <v-divider
-            class="mx-4"
-            inset
-            vertical
-          ></v-divider>
-          <v-spacer></v-spacer>
-                <v-text-field class="text-xs-center" v-model="search" append-icon="search" 
-                label="Búsqueda" single-line hide-details></v-text-field>
-                <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary"
-                dark
-                class="mb-2"
-                v-bind="attrs"
-                v-on="on"
-              >New Item</v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="headline">{{ formTitle }}</span>
-              </v-card-title>
-  
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.nombre" label="Dessert name"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-  
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.opciones="{ item }">
-        <v-icon
-          small
-          class="mr-2"
-          @click="editItem(item)"
-        >
-          mdi-pencil
-        </v-icon>
-        <v-icon
-          small
-          @click="deleteItem(item)"
-        >
-          mdi-delete
-        </v-icon>
-      </template>
-      <template v-slot:item.estado="{ item }">
-        <div v-if="item.estado">
-          <span class="blue--text">Activo</span>
-        </div>
-        <div v-else="item.estado">
-          <span class="red--text">Inactivo</span>
-        </div>
-<!--         {{ item.estado }} -->
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="listar()">Reset</v-btn>
-      </template>
-    </v-data-table>
+      <v-data-table :headers="headers" :items="categorias" :search="search" class="elevation-1">
+        <template v-slot:top>
+          <v-toolbar flat color="white">
+            <v-toolbar-title>Categorías</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-text-field class="text-xs-center" v-model="search" append-icon="search" label="Búsqueda" single-line
+              hide-details></v-text-field>
+            <v-spacer></v-spacer>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">Nuevo</v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="12" md="12">
+                        <v-text-field v-model="editedItem.nombre" label="Nombre"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="12" md="12">
+                        <v-text-field v-model="editedItem.descripcion" label="Descripción"></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">Cancelar</v-btn>
+                  <v-btn color="blue darken-1" text @click="guardar">Guardar</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
+          </v-toolbar>
+        </template>
+        <template v-slot:item.opciones="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)">
+            mdi-delete
+          </v-icon>
+        </template>
+        <template v-slot:item.estado="{ item }">
+          <div v-if="item.estado">
+            <span class="blue--text">Activo</span>
+          </div>
+          <div v-else="item.estado">
+            <span class="red--text">Inactivo</span>
+          </div>
+          <!--         {{ item.estado }} -->
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="listar()">Resetear</v-btn>
+        </template>
+      </v-data-table>
     </v-flex>
   </v-layout>
 </template>
@@ -104,8 +73,8 @@ export default {
   data() {
     return {
       dialog: false,
-      search:'',
-      categorias:[],
+      search: '',
+      categorias: [],
       headers: [
         { text: 'Opciones', value: 'opciones', sortable: false },
         { text: 'Nombre', value: 'nombre', sortable: true },
@@ -113,35 +82,31 @@ export default {
         { text: 'Estado', value: 'estado', sortable: false },
       ],
       editedIndex: -1,
-      editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
-      defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
+      _id: '',
+      nombre: '',
+      descripcion: '',
+     editedItem: {
+
+     },
+     defaultItem: {
+      nombre: '',
+      descripcion: '',
+     },
     };
   },
   computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    formTitle() {
+      return this.editedIndex === -1 ? 'Nuevo registro' : 'Editar registro'
     },
   },
 
   watch: {
-    dialog (val) {
+    dialog(val) {
       val || this.close()
     },
   },
 
-  created () {
+  created() {
     this.listar()
   },
 
@@ -150,37 +115,61 @@ export default {
       let me = this;
       axios.get('categoria/list').then(function (response) {
         me.categorias = response.data;
-      }).catch(function(error){
+      }).catch(function (error) {
         console.log(error);
       })
     },
 
-    editItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
+    editItem(item) {
+      this.editedIndex = this.categorias.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialog = true
     },
 
-    deleteItem (item) {
-      const index = this.desserts.indexOf(item)
-      confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+    deleteItem(item) {
+      const index = this.categorias.indexOf(item)
+      confirm('Estás seguro de borrar este registro?') && this.categorias.splice(index, 1)
     },
 
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
+    close() {
+      this.dialog = false;
+      /* this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
-      })
+      }) */
+      this.editedItem = Object.assign({}, this.defaultItem);
+      this.editedIndex = -1;
     },
 
-    save () {
+    save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        Object.assign(this.categorias[this.editedIndex], this.editedItem)
       } else {
-        this.desserts.push(this.editedItem)
+        this.categorias.push(this.editedItem)
       }
       this.close()
+    },
+    limpiar() {
+      this._id = '';
+      this.nombre = '';
+      this.descripcion = '';
+    },
+    guardar() {
+      let me = this;
+      if(this.editedIndex > -1) {
+        //Editar los datos del registro
+      } else {
+        //Guardar un nuevo registro
+        axios.post('categoria/add', { 'nombre': this.editedItem.nombre, 'descripcion': this.editedItem.descripcion })
+        .then(function(response) {
+          me.limpiar();
+          me.close();
+          me.listar();
+        }) 
+        .catch(function(error){
+          console.log(error);
+        });
+      }
     },
   },
 };
